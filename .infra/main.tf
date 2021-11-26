28d9af0b37dbbf4f2bb5dc6eb63a9b54ae5c3da3
peter_willmott <peter@aerobotics.com>
2021-11-26 15:17:34 +0200

    initial commit

--- .infra/main.tf
+++ .infra/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = ""
 }
 
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "aerobotics"
+    organization = ""
 
     workspaces {
       name = "jobs-asana-integrations"
@@ -19,12 +19,11 @@ terraform {
   }
 }
 
-
 data "terraform_remote_state" "lambda" {
   backend = "remote"
   config = {
     hostname = "app.terraform.io"
-    organization = "aerobotics"
+    organization = ""
     workspaces = {
       name = "terraform-aws-lambda"
     }
@@ -35,7 +34,7 @@ data "terraform_remote_state" "jobs" {
   backend = "remote"
   config = {
     hostname = "app.terraform.io"
-    organization = "aerobotics"
+    organization = ""
     workspaces = {
       name = "terraform-aws-jobs"
     }
@@ -48,69 +47,6 @@ resource "aws_ecr_repository" "ecr_repository" {
 }
 
 
-resource "aws_lambda_function" "survey_issues" {
-  image_uri        = var.image_uri
-  function_name    = "jobs-asana-integrations-survey-issues"
-  package_type     = "Image"
-  publish          = true
-  role             = data.terraform_remote_state.jobs.outputs.jobs_iam_role_arn
-  timeout          = 900
-
-  environment {
-    variables = {
-      SHERLOCK_GATEWAY_ACCESS_TOKEN = "AERO-INTERNAL-PASS"
-      JOBS_SECRET_ARN = data.terraform_remote_state.jobs.outputs.jobs_general_secret_arn
-    }
-  }
-
-  vpc_config {
-    security_group_ids = [
-      data.terraform_remote_state.jobs.outputs.sherlock_vpc_security_group_id
-    ]
-    // Need private subnets because of the NAT Gateway so that the lambda can connect to internet
-    subnet_ids = [
-      "subnet-0fd06654c739301f7",
-      "subnet-0b8fa19b7389a8a7d"
-    ]
-  }
-
-  image_config {
-    command = ["main.sync_survey_issues_to_asana_handler"]
-  }
-}
-
-resource "aws_lambda_function" "thermal_uploads" {
-  image_uri        = var.image_uri
-  function_name    = "jobs-asana-integrations-thermal-uploads"
-  package_type     = "Image"
-  publish          = true
-  role             = data.terraform_remote_state.jobs.outputs.jobs_iam_role_arn
-  timeout          = 900
-
-  environment {
-    variables = {
-      SHERLOCK_GATEWAY_ACCESS_TOKEN = "AERO-INTERNAL-PASS"
-      JOBS_SECRET_ARN = data.terraform_remote_state.jobs.outputs.jobs_general_secret_arn
-      UPDATE_TASK_ONLY_ON_COMPLETE_STATUS = "True"
-    }
-  }
-
-  vpc_config {
-    security_group_ids = [
-      data.terraform_remote_state.jobs.outputs.sherlock_vpc_security_group_id
-    ]
-    // Need private subnets because of the NAT Gateway so that the lambda can connect to internet
-    subnet_ids = [
-      "subnet-0fd06654c739301f7",
-      "subnet-0b8fa19b7389a8a7d"
-    ]
-  }
-
-  image_config {
-    command = ["main.sync_thermal_uploads_handler"]
-  }
-}
-
 resource "aws_lambda_function" "mapping_uploads" {
   image_uri        = var.image_uri
   function_name    = "jobs-asana-integrations-mapping-uploads"
@@ -121,61 +57,18 @@ resource "aws_lambda_function" "mapping_uploads" {
 
   environment {
     variables = {
-      SHERLOCK_GATEWAY_ACCESS_TOKEN = "AERO-INTERNAL-PASS"
+      SHERLOCK_GATEWAY_ACCESS_TOKEN = ""
       JOBS_SECRET_ARN = data.terraform_remote_state.jobs.outputs.jobs_general_secret_arn
       UPDATE_TASK_ONLY_ON_COMPLETE_STATUS = "True"
     }
   }
 
-  vpc_config {
-    security_group_ids = [
-      data.terraform_remote_state.jobs.outputs.sherlock_vpc_security_group_id
-    ]
-    // Need private subnets because of the NAT Gateway so that the lambda can connect to internet
-    subnet_ids = [
-      "subnet-0fd06654c739301f7",
-      "subnet-0b8fa19b7389a8a7d"
-    ]
-  }
 
   image_config {
     command = ["main.sync_mapping_uploads_handler"]
   }
 }
 
-resource "aws_cloudwatch_event_rule" "every_sixty_minutes" {
-  name = "trigger-every-60-minutes"
-  description = "Event which triggers every 60 minutes"
-  schedule_expression = "rate(60 minutes)"
-}
-
-resource "aws_cloudwatch_event_target" "trigger_survey_issues_every_sixty_minutes" {
-  rule = aws_cloudwatch_event_rule.every_sixty_minutes.name
-  target_id = aws_lambda_function.survey_issues.function_name
-  arn = aws_lambda_function.survey_issues.arn
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_survey_issues" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.survey_issues.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.every_sixty_minutes.arn
-}
-
-resource "aws_cloudwatch_event_target" "trigger_thermal_uploads_every_sixty_minutes" {
-  rule = aws_cloudwatch_event_rule.every_sixty_minutes.name
-  target_id = aws_lambda_function.thermal_uploads.function_name
-  arn = aws_lambda_function.thermal_uploads.arn
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_thermal_uploads" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.thermal_uploads.function_name
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.every_sixty_minutes.arn
-}
 
 resource "aws_cloudwatch_event_rule" "every_thirty_minutes" {
   name = "trigger-every-30-minutes"
--- README.md
+++ README.md
@@ -1,30 +1,28 @@
 # Asana Integrations
+
 This a repo of lightweight Asana integrations (lambda functions).
 
-## Survey issues
-A job which is responsible for keeping the 
-[Data-Ops | Tree Insights | Concernveys™](https://app.asana.com/0/1199123248405069/list) 
+## Mapping Uploads
+
+A job which is responsible for keeping the
+[Data-Ops | Mapping Uploads]
 Asana project
-up to date with survey issues. This is based off the following criteria:
-- Survey status 6 (In Progress) or 8 (Satellite Imagery Issue).
-- Latest internal job is errored or completed (if complete, another job should exist)
-- Survey past SLA date (> 3 days)
-- Latest internal job started > 7 hours ago and no end/error time
+up to date with mapping uploads. This is based off the following criteria:
 
-
-## Bitbucket PRs
-Currently not in this repo. To be added.
-
-## Google calendar
-Currently not in this repo. To be added.
-
+- Satellite
+- Drone (Aerobotics)
+- Self-service Drone
 
 # Testing
+
 Build
+
 ```
-docker build -t jobs-asana-integrations:latest . --build-arg PIP_EXTRA_INDEX_URL=$PIP_EXTRA_INDEX_URL 
+docker build -t jobs-asana-integrations:latest . --build-arg PIP_EXTRA_INDEX_URL=$PIP_EXTRA_INDEX_URL
 ```
+
 Run container
+
 ```
 docker run \
 -p 9000:8080 \
@@ -32,7 +30,9 @@ docker run \
 -v ~/.aero:/root/.aero \
 jobs-asana-integrations:latest
 ```
+
 Test container
+
 ```
 curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
 ```
